47706e9e7417356abfab50006d26b2959d4373da
ssaallmmoonn <127863452+ssaallmmoonn@users.noreply.github.com>
2025-03-23 11:27:22 +0800

    上传vue2笔记

---
+++ vue2_basic/19_单文件组件/School.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+// 这里是省略了Vue.extend，原来的写法是export default Vue.extend({})
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: '尚硅谷',
+            address: '北京昌平',
+        };
+    },
+    methods: {
+        showName() {
+            alert(this.name);
+        },
+    },
+};
+</script>
+
+<style>
+.demo {
+    background-color: orange;
+}
+</style>
