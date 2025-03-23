30dd7b2279d76de9ca48ce80d9f0a0834dd0244c
ssaallmmoonn <1241408850@qq.com>
2025-03-23 11:35:56 +0800

    添加vue2剩余笔记

---
+++ vue2_test/01_src_分析脚手架/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>init</div>
+</template>
+
+<script>
+export default {
+    name: `MyComponent`,
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue2_test/31_src_多级路由/router/index.js
@@ -0,0 +1,33 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+//引入组件
+import About from '../pages/About'
+import Home from '../pages/Home'
+import News from '../pages/News'
+import Message from '../pages/Message'
+
+//创建并暴露一个路由器
+export default new VueRouter({
+	routes:[
+		{
+			path:'/about',
+			component:About
+		},
+		{
+			path:'/home',
+			component:Home,
+			children:[
+				{
+					
+					path:'news',
+					component:News,
+				},
+				{
+					// 此处的path后不能加斜杠/
+					path:'message',
+					component:Message,
+				}
+			]
+		}
+	]
+})
